a4a032c2917532ac8a678aff66261bee1f6d356c
13360153 <13360153@me.mcu.edu.tw>
2026-04-19 04:40:46 +0000

    done

--- main.cpp
+++ main.cpp
@@ -15,26 +15,42 @@ public:
 
     // 回傳一顆好晶片的索引；若無法找到則回傳 -1
     int findGoodChip(vector<int> chips) {
-        // TODO:
-        // 反覆執行下列步驟：
-        //
-        // 1. 若 chips 中只剩一顆晶片，直接回傳它的索引
-        //
-        // 2. 令第一顆晶片為候選晶片 A
-        //
+    // 反覆執行
+    while (true) {
+        int n = chips.size();
+
+        // 1. 若只剩一顆
+        if (n == 1) {
+            return chips[0];
+        }
+
+        // 2. 第一顆為候選 A
+        int A = chips[0];
+
         // 3. 設 V = 0
-        //
-        // 4. 將 A 與其餘晶片逐一互測：
-        //    若 A 說 B 是好的，且 B 說 A 是好的，則 V++
-        //
+        int V = 0;
+
+        // 4. 與其他晶片互測
+        for (int i = 1; i < n; i++) {
+            int B = chips[i];
+
+            if (report[A][B] && report[B][A]) {
+                V++;
+            }
+        }
+
         // 5. 若 V >= n/2，回傳 A
-        //
-        // 6. 否則刪除 A，對剩下的晶片重複測試
+        if (V >= n / 2) {
+            return A;
+        }
 
-        return -1; // 請修改
+        // 6. 否則刪除 A
+        chips.erase(chips.begin());
     }
-};
 
+    return -1; // 理論上不會到這
+}
+};
 int main() {
     // 範例：6 顆晶片，編號 0~5
     // 假設好晶片多於壞晶片
